8d0a3d3fa571c6fa97fd8371d65bce2ed19c13fe
yan-batista <ybatistasouza@hotmail.com>
2024-01-18 19:03:39 -0300

    change index import in mediaController

--- server/src/controllers/MediaController/index.ts
+++ server/src/controllers/MediaController/index.ts
@@ -1,5 +1,5 @@
-import MediaRepository from "../../database/repositories/implementations/MediaRepository";
-import MediaService from "../../services/MediaServices";
+import MediaRepository from "../../database/repositories/implementations/mediaRepository";
+import MediaService from "../../services/mediaServices";
 import MediaController from "./mediaController";
 
 const mediaRepository: MediaRepository = new MediaRepository();
