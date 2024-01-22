0aebfd4f9a5f12ce0613c24ea8a913c584420e03
yan-batista <ybatistasouza@hotmail.com>
2024-01-22 15:50:50 -0300

    login and signup pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,30 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Nav from "./Components/Nav";
 import { HomePage } from "./Pages/Home";
+import LoginPage from "./Pages/Login";
 import { MoviesPage } from "./Pages/Movies";
 import SearchPage from "./Pages/Search";
 import { SeriesPage } from "./Pages/Series";
+import SignUpPage from "./Pages/SignUp";
 
 function App() {
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/series" element={<SeriesPage />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route element={<Nav />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/series" element={<SeriesPage />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Route>
+          <Route>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ client/src/Pages/Login.tsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+import { LogoIcon } from "../Components/Icons";
+
+const LoginPage = () => {
+  return (
+    <section className="w-full h-full flex flex-col items-center justify-center gap-9">
+      <LogoIcon className="w-8 h-8 fill-logo lg:w-8 lg:h-8" viewbox={"0 0 33 27"} />
+      <div className="bg-side rounded-md p-6 flex flex-col items-center">
+        <h1 className="text-xl w-full text-left">Login</h1>
+        <form className="flex flex-col gap-4 mt-4">
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="bg-transparent border-b border-icon py-4 outline-none indent-4"
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            className="bg-transparent border-b border-icon py-4 outline-none indent-4"
+          />
+          <input type="submit" value="Login to your account" className="bg-logo text-white px-8 py-4 rounded-md my-5" />
+        </form>
+        <p>
+          Don't have an account?{" "}
+          <NavLink to="/signup" className="text-logo">
+            Sign Up
+          </NavLink>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
